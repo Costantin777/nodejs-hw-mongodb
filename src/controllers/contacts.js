e7ff70864330ef7d27dc2ce6a859d2ab099c9d8c
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-06-08 15:02:48 +0300

    new

---
+++ src/controllers/contacts.js
@@ -0,0 +1,25 @@
+import { getAllContacts, getContactById } from '..//services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+  res.status(200).json({
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { id } = req.params;
+
+  const contact = await getContactById(id);
+
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.status(200).json({
+    message: `Successfully found contact with id ${id}!`,
+    data: contact,
+  });
+};
---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler, notFoundHandler } from './middlewares/errorHandler.js';
+
+export const setupServer = () => {
+  const app = express();
+  // const PORT = 3000(process.env.PORT);
+  const PORT = Number(env('PORT', '3000'));
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use((req, res, next) => {
+    console.log(`Time: ${new Date().toLocaleString()}`);
+    next();
+  });
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use(contactsRouter);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
