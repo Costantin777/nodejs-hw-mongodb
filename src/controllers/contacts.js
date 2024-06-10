d9c69449d3ffbde72ecc510727c76ff6a7ba1123
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-06-10 12:10:41 +0300

    new

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,25 +1,34 @@
-import { getAllContacts, getContactById } from '..//services/contacts.js';
+import { getAllContacts, getContactById } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
-export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
-  res.status(200).json({
-    message: 'Successfully found contacts!',
-    data: contacts,
-  });
+export const getContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    next(createHttpError(500, 'Error getting contacts'));
+  }
 };
 
 export const getContactByIdController = async (req, res, next) => {
   const { id } = req.params;
 
-  const contact = await getContactById(id);
+  try {
+    const contact = await getContactById(id);
 
-  if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    if (!contact) {
+      next(createHttpError(404, 'Contact not found'));
+      return;
+    }
+
+    res.status(200).json({
+      message: `Successfully found contact with id ${id}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(createHttpError(500, `Error getting contact with id ${id}`));
   }
-  res.status(200).json({
-    message: `Successfully found contact with id ${id}!`,
-    data: contact,
-  });
 };
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,23 +4,20 @@ export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.name,
+      message: err.message,
       data: err,
     });
-    return;
+  } else {
+    res.status(500).json({
+      status: 500,
+      message: 'Something went wrong',
+      data: err.message,
+    });
   }
-
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
-  });
   next();
 };
 
-export const notFoundHandler = (err, req, res, next) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-  next();
+export const notFoundHandler = (req, res, next) => {
+  const error = new HttpError(404, 'Route not found');
+  next(error);
 };
