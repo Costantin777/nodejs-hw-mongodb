bea70bd288bc4b384e7343e7f20407b6cb7c8b14
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-07-02 20:56:04 +0300

    new

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,13 +1,17 @@
 import Joi from 'joi';
 
-const createContactSchema = Joi.object({
+export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email(),
   phoneNumber: Joi.string().min(3).max(20).required(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
-const updateContactSchema = Joi.object({
+export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  email: Joi.string().email(),
   phoneNumber: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
-
-export { createContactSchema, updateContactSchema };
