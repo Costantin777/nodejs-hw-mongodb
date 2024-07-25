05f08ddb60523b67e72dbd09507e288c1ec6543c
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-07-25 15:45:32 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,25 +1,24 @@
 import express from 'express';
-import pino from 'pino-http';
 import cors from 'cors';
-import { env } from './utils/env.js';
-import router from './routers/index.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
-import { UPLOAD_DIR } from './constants/constans.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { env } from './utils/env.js';
+import rootRouter from './routers/index.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { UPLOAD_DIR } from './constants/constants.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
-
 export const setupServer = () => {
   const app = express();
-
-  app.use(express.json());
-
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+  app.use(cookieParser());
+  // add cors
   app.use(cors());
 
-  app.use(cookieParser());
-
+  // add pino
   app.use(
     pino({
       transport: {
@@ -28,16 +27,23 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+  // parser json
+  app.use(
+    express.json({
+      limit: '1mb',
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
 
-  app.use(router);
+  app.use(rootRouter);
 
-  app.use('*', notFoundHandler);
+  app.use(notFoundHandler);
 
   app.use(errorHandler);
 
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}!`);
   });
+
+  return app;
 };
