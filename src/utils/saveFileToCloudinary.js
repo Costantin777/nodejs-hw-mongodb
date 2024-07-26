8a8aa6bd5f0e1b2643e0605d011359294cef8b25
Costantin777 <145683428+Costantin777@users.noreply.github.com>
2024-07-26 13:27:36 +0300

    new

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,16 +1,25 @@
 import cloudinary from 'cloudinary';
+import dotenv from 'dotenv';
 
+// Імпортуйте модуль env з файлу './env.js'
 import { env } from './env.js';
-import { CLOUDINARY } from '../constants/index.js';
 
+// Встановіть конфігурацію для Cloudinary
 cloudinary.v2.config({
   secure: true,
-  cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
+  cloud_name: env('CLOUDINARY_NAME'), // Використовуйте функцію env для отримання значення
+  api_key: env('API_KEY'), // Використовуйте функцію env для отримання значення
+  api_secret: env('API_SECRET'), // Використовуйте функцію env для отримання значення
 });
 
+// Функція для завантаження файлу на Cloudinary
 export const saveFileToCloudinary = async file => {
-  const response = await cloudinary.v2.uploader.upload(file.path);
-  return response.secure_url;
+  try {
+    const response = await cloudinary.v2.uploader.upload(file.path);
+    return response.secure_url;
+  } catch (error) {
+    console.error('Помилка при завантаженні на Cloudinary:', error.message);
+    throw error;
+  }
 };
+import { config } from 'dotenv';
